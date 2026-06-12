44fa18fbfe859c32a75858440d3814fa831e68ce
Patelvivek24 <vivek@inventam.com>
2026-06-12 13:33:52 +0530

    create a hero section problrm section solution section

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: D:\agriintel-ai-website\src\app\page.tsx
+// File: D:\My self\agriintel-ai-website\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
